89c2ff7a0ba8522c3c1a85e6c71646448cbfb665
kevin_abr <kevin.abraham@almullaexchange.com>
2025-01-11 12:41:52 +0530

    first blood

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <!-- //check router/index.js -->
+    <router-view />
+</template>
+
+<style scoped></style>
---
+++ src/layout/composables/layout.js
@@ -0,0 +1,88 @@
+import { computed, reactive, ref } from 'vue';
+
+const layoutConfig = reactive({
+    preset: 'Aura',
+    primary: 'emerald',
+    surface: null,
+    darkTheme: true,
+    menuMode: 'static'
+});
+
+// Profile menu state
+const profileMenu = ref(null);
+
+const layoutState = reactive({
+    staticMenuDesktopInactive: false,
+    overlayMenuActive: false,
+    profileSidebarVisible: false,
+    configSidebarVisible: false,
+    staticMenuMobileActive: false,
+    menuHoverActive: false,
+    activeMenuItem: null
+});
+
+export function useLayout() {
+    const setActiveMenuItem = (item) => {
+        layoutState.activeMenuItem = item.value || item;
+    };
+
+    const toggleDarkMode = () => {
+        if (!document.startViewTransition) {
+            executeDarkModeToggle();
+
+            return;
+        }
+
+        document.startViewTransition(() => executeDarkModeToggle(event));
+    };
+
+    const executeDarkModeToggle = () => {
+        layoutConfig.darkTheme = !layoutConfig.darkTheme;
+        document.documentElement.classList.toggle('app-dark');
+    };
+
+    const toggleMenu = () => {
+        if (layoutConfig.menuMode === 'overlay') {
+            layoutState.overlayMenuActive = !layoutState.overlayMenuActive;
+        }
+
+        if (window.innerWidth > 991) {
+            layoutState.staticMenuDesktopInactive = !layoutState.staticMenuDesktopInactive;
+        } else {
+            layoutState.staticMenuMobileActive = !layoutState.staticMenuMobileActive;
+        }
+    };
+
+    const togglePanel = (event) => {
+        if (profileMenu.value) {
+            profileMenu.value.toggle(event);
+        }
+    };
+
+    const logout = () => {
+        // Perform logout logic here (e.g., clear session, redirect to login page)
+        console.log("User logged out");
+    };
+
+    const isSidebarActive = computed(() => layoutState.overlayMenuActive || layoutState.staticMenuMobileActive);
+
+    const isDarkTheme = computed(() => layoutConfig.darkTheme);
+
+    const getPrimary = computed(() => layoutConfig.primary);
+
+    const getSurface = computed(() => layoutConfig.surface);
+
+    return {
+        layoutConfig,
+        layoutState,
+        toggleMenu,
+        isSidebarActive,
+        isDarkTheme,
+        getPrimary,
+        getSurface,
+        setActiveMenuItem,
+        toggleDarkMode,
+        togglePanel,
+        logout
+    };
+}
